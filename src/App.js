d41887b3e29ec0a336ee922ca59e40a3477ced01
muhammederamala <muhammederamala15@gmail.com>
2023-08-09 15:39:36 +0530

    modified:   package-lock.json 	modified:   package.json 	new file:   public/images/airpods.jpg 	new file:   public/images/alexa.jpg 	new file:   public/images/camera.jpg 	new file:   public/images/mouse.jpg 	new file:   public/images/phone.jpg 	new file:   public/images/playstation.jpg 	new file:   public/images/sample.jpg 	modified:   src/App.js 	new file:   src/components/Product.js 	new file:   src/components/Rating.js 	modified:   src/index.css 	new file:   src/products.js 	new file:   src/screens/HomeScreen.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import { Container } from 'react-bootstrap';
+import { createBrowserRouter , createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+        <Route index element={<HomeScreen />} />
+  )
+)
 
 function App() {
   return (
     <div>
       <Header />
-      <main>
-        Welcome
+      <main className='py-3'>
+        <Container>
+          <RouterProvider router={router} />
+        </Container>
       </main>
       <Footer />
     </div>
---
+++ src/components/Rating.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Rating({value, text, color}) {
+  return (
+    <div className='rating'>
+      <span>
+        <i style={{color}} classname={
+            value >= 1
+                ? 'fas fa-star'
+                :value >= 0.5
+                    ? 'fas fa-star-half-alt'
+                    :'far fa-star'
+        }>
+
+        </i>
+      </span>
+      <span>
+        {text && text}
+      </span>
+    </div>
+  )
+}
+
+export default Rating
